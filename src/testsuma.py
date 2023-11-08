fb698fe89cf5c7a44f906370943c4b19df7ccb50
barbiepavez <b.v.p.m.bp@gmail.com>
2023-11-08 17:10:35 -0300

    test para verificar que la funcion add esta ok

--- src/testsuma.py
+++ src/testsuma.py
@@ -1,20 +1,25 @@
-import math
-def add(a, b):
-    return a + b
+import unittest  # Importa el módulo unittest para realizar pruebas unitarias.
+from funcionesbasicas import add  # Importa la función 'add' desde el módulo 'funcionesbasicas'.
 
-def menu():
-    print("Select operation:")
-    print("1. Suma")
+# Define una clase llamada 'TestFuncionesBasicas' que hereda de 'unittest.TestCase'.
+class TestFuncionesBasicas(unittest.TestCase):
+    
+    # Define un método de prueba llamado 'test_suma' para probar la función 'add' con argumentos 2 y 3.
+    def test_suma(self):
+        self.assertEqual(add(2, 3), 5)  # Comprueba si add(2, 3) es igual a 5.
 
-def perform_operation(choice):
-    if choice == 1:
-        a = float(input("Enter first number: "))
-        b = float(input("Enter second number: "))
-        print("Result: ", add(a, b))
-def main():
-    while True:
-        choice = menu()
-        perform_operation(choice)
+    # Define otro método de prueba 'test_suma' para probar la función 'add' con argumentos -5 y -7.
+    def test_suma(self):
+        self.assertEqual(add(-5, -7), -12)  # Comprueba si add(-5, -7) es igual a -12.
 
-if __name__ == "__main__":
-    main() 
+    # Define otro método de prueba 'test_suma' para probar la función 'add' con argumentos 10 y -4.
+    def test_suma(self):
+        self.assertEqual(add(10, -4), 6)  # Comprueba si add(10, -4) es igual a 6.
+
+    # Define otro método de prueba 'test_suma' para probar la función 'add' con argumentos -12 y 5.
+    def test_suma(self):
+        self.assertEqual(add(-12, 5), -7)  # Comprueba si add(-12, 5) es igual a -7.
+
+# Verifica si el script se ejecuta como el programa principal.
+if __name__ == '__main__':
+    unittest.main()  # Ejecuta todas las pruebas definidas en la clase 'TestFuncionesBasicas'.
