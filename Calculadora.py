efb248592c8a38851276c9945a575b5e0864efcc
EileenSaavedra <149902494+EileenSaavedra@users.noreply.github.com>
2023-11-08 11:26:07 -0300

    Update Calculadora.py
    Este feature permite restar números.

--- Calculadora.py
+++ Calculadora.py
@@ -2,18 +2,29 @@ import math
 
 def suma(a, b):
     return a + b
-   
+    
+def subtract(a, b):
+    return a - b
+    
 def menu():
     print("Select operation:")
     print("1. Suma")
-   
+    print("2. Resta")
+    
 choice = int(input("Enter choice: "))
     return choice
+
 def perform_operation(choice):
     if choice == 1:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
-        print("Result: ", add(a, b))   
+        print("Result: ", add(a, b))  
+        
+     elif choice == 2:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+        print("Result: ", subtract(a, b))
+    
 def main():
     while True:
         choice = menu()
