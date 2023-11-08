e81a8792921272088ed3aec80a8fbe9f9b161785
EileenSaavedra <149902494+EileenSaavedra@users.noreply.github.com>
2023-11-08 12:56:40 -0300

    Update funcionesbasicas.py
    Este feature permite multiplicar

--- src/funcionesbasicas.py
+++ src/funcionesbasicas.py
@@ -2,15 +2,24 @@ import math
 def add(a, b):
     return a + b
 
+def multiply(a, b):
+    return a * b
+
 def menu():
     print("Select operation:")
     print("1. Suma")
+    print("3. Multiplicacion:")
 
 def perform_operation(choice):
     if choice == 1:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", add(a, b))
+
+     elif choice == 3:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+        print("Result: ", multiply(a, b))
 def main():
     while True:
         choice = menu()
