a916af044761983033624467fd202b8dc94c4eeb
barbiepavez <b.v.p.m.bp@gmail.com>
2023-11-08 18:33:55 -0300

    hotfix alineacion codigo

--- src/funcionesbasicas.py
+++ src/funcionesbasicas.py
@@ -21,11 +21,14 @@ def remainder(a, b):
 def menu():
     print("Select operation:")
     print("1. Suma")
-    Print("2. resta")
+    print("2. resta")
     print("3. Multiplicación")
     print("4. division")
     print("5. Cuociente")
     print("6. resto ")
+
+    choice = int(input("Enter choice: "))
+    return choice
 
 def perform_operation(choice):
     if choice == 1:
@@ -39,22 +42,22 @@ def perform_operation(choice):
         b = float(input("Enter second number: "))
         print("Result: ", subtract(a, b))
 
-   elif choice == 3:
+    elif choice == 3:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", multiply(a, b))
 
-   elif choice == 4:
+    elif choice == 4:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", divide(a, b))
 
-   elif choice == 5:
+    elif choice == 5:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", quotient(a, b))
 
-  elif choice == 6:
+    elif choice == 6:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", remainder(a, b))
