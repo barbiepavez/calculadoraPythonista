03ff464a632560c2b122707362ff0550c45e4310
barbiepavez <149853412+barbiepavez@users.noreply.github.com>
2023-11-08 11:55:10 -0400

    Update funcionesbasicas.py
    este feature permite restar numeros

--- src/funcionesbasicas.py
+++ src/funcionesbasicas.py
@@ -1,16 +1,26 @@
 import math
 def add(a, b):
     return a + b
-
+    
+def substract(a, b):
+    return a - b
+    
 def menu():
     print("Select operation:")
     print("1. Suma")
+    print("2. Resta")
 
 def perform_operation(choice):
     if choice == 1:
         a = float(input("Enter first number: "))
         b = float(input("Enter second number: "))
         print("Result: ", add(a, b))
+
+    if choice == 2:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+        print("Result: ", substract(a, b))
+        
 def main():
     while True:
         choice = menu()
